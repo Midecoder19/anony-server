497f3784564290e25b8011675411c3daae47fe9d
Ayomide <ayomidebabarinde07@gmail.com>
2024-11-26 12:24:24 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,26 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Serve static files from the React app
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-// Handle React routing, return all requests to React app
+
+// React SPA routing
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
+// Error Handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server Running in port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
